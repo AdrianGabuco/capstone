77894c742d63629d93cfa23fe578c0a4b5dd76b6
Account1 <xpkenjojo995599@gmail.com>
2024-10-29 01:57:52 +0800

    TESTING for CSS

--- chmc_wbms/chmc_wbms/settings.py
+++ chmc_wbms/chmc_wbms/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/public/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/public')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
